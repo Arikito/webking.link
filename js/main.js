94234f343072bee1518f8a79b5451b72b721273a
Alexander <arikito.91@gmail.com>
2015-11-08 02:26:59 +0200

    header + responsive

--- js/main.js
+++ js/main.js
@@ -8,6 +8,28 @@ $(function(){
 	$('#page_wrap > section').css({
 		"height": $(window).height()
 	});
+
+	$('a[href*=#]:not([href=#])').on('click', function(e){
+		if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+			var target = $(this.hash);
+			target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+			if (target.length) {
+				$('html,body').stop(true, true).animate({
+					scrollTop: target.offset().top
+				}, 600);
+				return false;
+			}
+		}
+	});
+
+	$(window).scroll(function(){
+		if($(this).scrollTop() >= $('nav').offset().top+$('nav').outerHeight() && $('.fixed_nav').is(":not(:visible)")){
+			$('.fixed_nav').fadeIn('slow');
+			console.log($(this).scrollTop());
+		}else if($(this).scrollTop() < $('nav').offset().top+$('nav').outerHeight() && $('.fixed_nav').is(":visible")){
+			$('.fixed_nav').fadeOut('slow');
+		}
+	});
 	// $('.map').scroll(function(e){
 	// 	e.preventDefault;
 	// 	return false;
